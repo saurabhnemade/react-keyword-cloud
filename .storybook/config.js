d9f789fbf278bf36f7bf62b5bf71158c3f541f04
Saurabh Nemade <saurabhnemade@users.noreply.github.com>
2019-05-08 23:38:36 +0530

    storybook updated

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,19 @@
 import { configure } from '@storybook/react';
 
+import { setOptions } from '@storybook/addon-options';
+
+setOptions({
+  name: 'React Keyword Cloud',
+  url: '#',
+  showStoriesPanel: true,
+  showAddonPanel: true,
+  addonPanelInRight: true,
+  selectedAddonPanel: "Backgrounds",
+  header: false, // Toggles display of header with component name and description
+  inline: false, // Displays info inline vs click button to view
+  source: true,
+});
+
 function loadStories() {
   require('../src/stories');
 }
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,28 +1,13 @@
 import React from 'react';
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf,addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 import KeywordCloud from "./../index";
 import generateRandomData from "./data";
 
-import { Button, Welcome } from '@storybook/react/demo';
+addDecorator(withInfo);
 
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
-
-
-storiesOf("KeyWord Cloud", module)
+storiesOf("Keyword Cloud", module)
   .add("Default minimum", () => (
     <KeywordCloud data={generateRandomData()}/>
   ))
